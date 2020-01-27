e6bb44294951e8e5169d3053eceb244568c847a1
Zac Pullar-Strecker <zacpullarstrecker@catalyst.net.nz>
2020-01-28 10:41:07 +1300

    Add tests for localhost and other URIs

--- oidc_provider/tests/cases/test_allowed_redirect_uri.py
+++ oidc_provider/tests/cases/test_allowed_redirect_uri.py
@@ -4,21 +4,34 @@ from oidc_provider.tests.app.utils import create_fake_client
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("allowed_redirect_uris, redirect_uri, expected", [
+    # Exact match
     (
         ["https://user:password@example.com?param=value#fragment"],
         "https://user:password@example.com?param=value#fragment",
         True
     ),
+    # Normal fail
     (
         ["https://user:password@example.com?param=value#fragment"],
         "https://user:password@example.com?param=bad#fragment",
         False
     ),
+    # Accepts one of list
     (["https://a.com", "https://b.com", "https://c.com"], "https://a.com", True),
+    # Port ignored for loopback (v4)
     (["127.0.0.1"], "127.0.0.1:5000", True),
+    # Port ignored for loopback (v6)
     (["[::1]"], "[::1]:5000", True),
+    # Port specified in config ignored (v6)
     (["[::1]:5000"], "[::1]", True),
-    (["::]", "[::1]:5000"], "[::1]:3000", True)
+    # Configured URI which could not be parsed falls back to others
+    (["::]", "[::1]:5000"], "[::1]:3000", True),
+    # Localhost not accepted as loopback
+    (["localhost"], "localhost:3000", False),
+    # Custom URI schemes accepted (exact match)
+    (["customscheme://openid/callback"], "customscheme://openid/callback", True),
+    # Claimed https (reverse-DNS) accepted
+    (["https://com.example.app/callback"], "https://com.example.app/callback", True),
 ])
 def test_redirect_uri(allowed_redirect_uris, redirect_uri, expected):
     client = create_fake_client("code")
