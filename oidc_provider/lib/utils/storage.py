b519ad0d050b9b8a349eddffee8cdf7087bde172
Zac Pullar-Strecker <zacpullarstrecker@catalyst.net.nz>
2020-01-28 16:52:12 +1300

    Add migration & fix flake8

--- oidc_provider/lib/utils/storage.py
+++ oidc_provider/lib/utils/storage.py
@@ -6,7 +6,9 @@ from oidc_provider import settings
 location = settings.get('OIDC_RSA_CERT_LOCATION')
 
 if settings.get('OIDC_RSA_CERT_STORE') == 'filesystem' and location is None:
-    raise ImproperlyConfigured("OIDC_RSA_CERT_LOCATION must be configured if OIDC_RSA_CERT_STORE='filesystem'")
+    raise ImproperlyConfigured(
+        "OIDC_RSA_CERT_LOCATION must be configured if OIDC_RSA_CERT_STORE='filesystem'")
+
 
 class KeyStorage(FileSystemStorage):
     location = location
